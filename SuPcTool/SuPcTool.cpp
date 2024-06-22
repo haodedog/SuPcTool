3ba61c3450c6de43d1f775c9b0dc184a5a027c7a
sunyanx <sun_x_chuan123@outlook.com>
2024-06-22 22:47:05 +0800

    开始这个项目

---
+++ SuPcTool/SuPcTool.cpp
@@ -0,0 +1,8 @@
+﻿#include<Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+	static TCHAR szAppName[] = TEXT("SuPcTool");
+	MessageBox(NULL, TEXT("作者正在努力制作哦，请稍等..."), TEXT("SuPcTool"), MB_OK);
+
+}
