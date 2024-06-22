d0b9f58683b3892cc88fd6fcc6a65dce89abe53b
sunyanx <sun_x_chuan123@outlook.com>
2024-06-22 22:51:18 +0800

    修复一个显示bug，新增关闭确认功能

--- SuPcTool/SuPcTool.cpp
+++ SuPcTool/SuPcTool.cpp
@@ -1,8 +1,81 @@
 ﻿#include<Windows.h>
 
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
 {
-	static TCHAR szAppName[] = TEXT("SuPcTool");
-	MessageBox(NULL, TEXT("作者正在努力制作哦，请稍等..."), TEXT("SuPcTool"), MB_OK);
+    static TCHAR szAppName[] = TEXT("suPcTool");
+    //MessageBox(NULL, TEXT("作者正在努力制作哦，请稍等..."), TEXT("suPcTool"), MB_OK);
+    HWND hwnd;
+    MSG msg;
+    WNDCLASS wndclass;
 
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hInstance = hInstance;
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndclass.lpszMenuName = NULL;
+    wndclass.lpszClassName = szAppName;
+
+    if (!RegisterClass(&wndclass))
+    {
+        MessageBox(NULL, TEXT("The exe need Windows NT !"), szAppName, MB_OK);
+        return 0;
+    }
+
+    hwnd = CreateWindow(szAppName,
+        TEXT("su电脑工具箱"),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+
+    ShowWindow(hwnd, iCmdShow);
+    UpdateWindow(hwnd);
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    HDC hdc;
+    PAINTSTRUCT ps;
+    RECT rect;
+
+    switch (message)
+    {
+    case WM_PAINT:
+        hdc = BeginPaint(hwnd, &ps);
+        GetClientRect(hwnd, &rect);
+        DrawText(hdc, TEXT("su电脑工具箱正在开发中..."), -1, &rect,
+            DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+        EndPaint(hwnd, &ps);
+        return 0;
+    case WM_CLOSE:
+        MessageBox(hwnd, TEXT("确认"), TEXT("确认关闭suPcTool？"), MB_OK);
+        DestroyWindow(hwnd);
+        return 0;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    return DefWindowProc(hwnd, message, wParam, lParam);
 }
